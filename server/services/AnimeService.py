8414055227be6b92f1179d7d9306d0734f10282d
leeroygomos <leeroy.gomos@gmail.com>
2023-05-29 21:09:59 -0700

    updated json return values

--- server/services/AnimeService.py
+++ server/services/AnimeService.py
@@ -5,7 +5,7 @@ from models.Character import Character
 class AnimeService:
     def getAnime(slug):
         anime = Anime.query.filter_by(slug=slug).first_or_404()
-        return jsonify(anime.serialize)
+        return jsonify({'anime': [anime.serialize]})
     
     def getAllAnime():
         animes = Anime.query.all()
@@ -14,4 +14,4 @@ class AnimeService:
     def getAllAnimeCharacters(slug):
         anime = Anime.query.filter_by(slug=slug).first()
         characters = Character.query.filter_by(anime_id=anime.id).all()
-        return jsonify([character.serialize for character in characters])
+        return jsonify({'anime': [character.serialize for character in characters]})
--- server/services/CharacterService.py
+++ server/services/CharacterService.py
@@ -4,8 +4,8 @@ from models.Character import Character
 class CharacterService:
     def getCharacter(id):
         character = Character.query.filter_by(id=id).first()
-        return jsonify(character.serialize)
+        return jsonify({'character': [character.serialize]})
     
     def getAllCharacters():
         characters = Character.query.all()
-        return jsonify([character.serialize for character in characters])
+        return jsonify({'character': [character.serialize for character in characters]})
